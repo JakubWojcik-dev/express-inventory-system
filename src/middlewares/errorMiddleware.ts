f6397056afd17dde9837858df28751c0e58d7e9f
JakubWojcik-dev <Jakub-mail1@o2.pl>
2025-01-19 22:59:53 +0100

    feat: add json error middleware handler

---
+++ src/middlewares/errorMiddleware.ts
@@ -0,0 +1,16 @@
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { MongoClient } from "mongodb";
+import { IError } from "../types/error";
+
+export const errorMiddleware = () => {
+  return (err: IError, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof SyntaxError && "body" in err) {
+      res.status(400).json({
+        message:
+          "Invalid data format. Only JSON format or empty data in GET method case",
+        error: err.message,
+      });
+    }
+    next(err);
+  };
+};
