afb27f72ba50125c1b9bac569e50a80e6955fc0f
JakubWojcik-dev <Jakub-mail1@o2.pl>
2025-01-20 23:34:51 +0100

    fix: main server file

--- src/server.ts
+++ src/server.ts
@@ -2,20 +2,24 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import productsCommandRouter from "./commands/productsCommand";
+import stockCommand from "./commands/stockCommand";
 import connectToDb, {
   closeConnectionOnExit,
 } from "./database/router/db.connection";
 import { dbMiddleware } from "./middlewares/dbMiddleware";
+import productsQueriesRouter from "./queries/productsQueries";
 import { MongoClient } from "mongodb";
 import { errorMiddleware } from "./middlewares/errorMiddleware";
-
+import orderCommandRouter from "./commands/orderCommand";
+import { setupSwagger } from "./swagger";
+import orderQueriesRouter from "./queries/ordersQueries";
 const app = express();
 let dbConnection: MongoClient | undefined;
 
 app.use(cors());
 app.use(express.json());
 app.use(errorMiddleware());
-
+setupSwagger(app);
 const server = async () => {
   try {
     dbConnection = await connectToDb();
@@ -26,7 +30,13 @@ const server = async () => {
   }
   app.use(dbMiddleware(dbConnection));
 
-  app.post("/products", productsCommandRouter);
+  app.use("/orders", orderCommandRouter);
+  app.use("/orders", orderQueriesRouter);
+  app.use("/products", stockCommand);
+
+  app.use("/products", productsCommandRouter);
+  app.put("/products", productsCommandRouter);
+  app.use("/products", productsQueriesRouter);
 
   app.listen(process.env.PORT, () => {
     console.log(`Server is running on http://localhost:${process.env.PORT}`);
